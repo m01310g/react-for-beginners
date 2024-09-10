8349ff5a5da5b87f2af81170a2b4158a33d1b06d
m01310g <m01310g@gmail.com>
2024-09-10 17:45:55 +0900

    Cleanup Function

--- src/App.js
+++ src/App.js
@@ -1,37 +1,29 @@
+import { func } from "prop-types";
 import styles from "./App.module.css"
 import { useState, useEffect } from "react";
 
-// useEffect의 첫번째 인자: 한 번만 실행시키고 싶은 코드
-// 두번째 인자: deps(대괄호([]) 내에 있는 변수가 변할 때 코드 실행)
+function Hello() {
+  function byeFn() {
+    console.log("bye :(")
+  }
+  
+  function hiFn() {
+    console.log("created :)");
+    return byeFn;
+  }
+
+  // Cleanup function: component가 destroy 될 때 실행되도록 할 수 있음
+  useEffect(hiFn, [])
+  return <h1>Hello</h1>;
+}
+
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () =>  setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-
-  useEffect(() => {    // 오직 한 번만 실행
-    console.log("CALL THE API...");
-  }, []);
-  useEffect(() => {
-    console.log("I run when 'keyword' changes");
-  }, [keyword]);
-  useEffect(() => {
-    console.log("I run when 'counter' changes");
-  }, [counter]);
-  useEffect(() => {
-    console.log("I run when keyword & counter changes");
-  }, [keyword, counter]);
-
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input 
-        value={keyword}
-        onChange={onChange} 
-        type="text" 
-        placeholder="Search here" 
-      />
-      <h1 className={styles.title}>{counter}</h1>
-      <button onClick={onClick}>Click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
