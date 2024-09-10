fba48412ad23b02ed0b033e03b1df4f8e01c98ed
m01310g <m01310g@gmail.com>
2024-09-10 17:29:37 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from "./App.module.css"
+import { useState, useEffect } from "react";
 
+// useEffect의 첫번째 인자: 한 번만 실행시키고 싶은 코드
+// 두번째 인자: deps(대괄호([]) 내에 있는 변수가 변할 때 코드 실행)
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () =>  setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+
+  useEffect(() => {    // 오직 한 번만 실행
+    console.log("CALL THE API...");
+  }, []);
+  useEffect(() => {
+    console.log("I run when 'keyword' changes");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes");
+  }, [counter]);
+  useEffect(() => {
+    console.log("I run when keyword & counter changes");
+  }, [keyword, counter]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input 
+        value={keyword}
+        onChange={onChange} 
+        type="text" 
+        placeholder="Search here" 
+      />
+      <h1 className={styles.title}>{counter}</h1>
+      <button onClick={onClick}>Click me</button>
     </div>
   );
 }
