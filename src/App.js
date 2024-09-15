2f0abb41b7d7b7fb805901bd3a5ff8d5ae93a69f
m01310g <m01310g@gmail.com>
2024-09-15 15:48:30 +0900

    coin tracker

--- src/App.js
+++ src/App.js
@@ -1,43 +1,63 @@
-import { useState } from "react";
+import { number } from "prop-types";
+import { useEffect, useState } from "react";
 
+// input을 받고, option 태그를 이용해 USD -> BTC로 변환하는 코드 작성
 function App() {
-  const [toDo, setTodo] = useState("");
-  const [toDos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(true);
+  // 빈 array 값 넘겨줌
+  const [coins, setCoins] = useState([]);
+  const [asset, setAsset] = useState(0);
+  const [price, setPrice] = useState(0);
+  const [name, setName] = useState("");
 
-  const onChange = (event) => setTodo(event.target.value);
+  // 배열이 비어 있으면 단 한 번만 작동
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+    .then((response) => response.json())
+    .then((json) => {
+      setCoins(json);
+      setLoading(false);
+    });
+  }, []);
+
+  const onChange = (event) => {
+    setName(event.target.selectedOptions[0].text.split(":")[0]);
+    setPrice(parseFloat(event.target.value));
+  }
+
   const onSubmit = (event) => {
     event.preventDefault();
-    // toDo가 비어 있다면 함수가 작동하지 않도록 함
-    if (toDo == "") {
-      return;
-    }
-
-    // currentArray를 받아오고 새로운 array를 추가해줌
-    // [(array), ...(other array)]: array 내에 새로운 요소를 추가
-    setTodos((currentArray) => [toDo, ...currentArray]);
-
-    // input 창을 비워줌
-    setTodo("");
-  };
-
-  // map 함수에서 첫번째 인자는 배열 내의 각 요소를 의미함
+    if (asset === 0) return;
+    setAsset(0);
+  }
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input 
-          onChange={onChange} 
-          value={toDo} 
-          text="Text" 
-          placeholder="Write your to do" 
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>))}
-      </ul>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <div>
+          <select onChange={onChange}>
+            {coins.map((coin) => (
+              <option key={coin.id} value={coin.quotes.USD.price}>
+                {coin.name}({coin.symbol}): ${coin.quotes.USD.price} USD
+              </option>
+            ))}
+          </select>
+          <div>
+            <form onSubmit={onSubmit}>
+              <input 
+                onChange={(event) => setAsset(event.target.value)} 
+                value={asset === 0? "" : asset}  
+                type="number"
+                placeholder="How much do you have?"
+              />
+              <button>Reset</button>
+            </form>
+          </div>
+          <h2>With ${asset === undefined ? 0 : asset}, you can purchase {asset > 0 && price !== 0 ? (asset / price) : 0} of {name}</h2>
+        </div>
+      )}
     </div>
   );
 }
